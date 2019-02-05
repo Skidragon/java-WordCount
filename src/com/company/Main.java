7f9e09c477890d05502181346e447453f0f40df6
Simon Davis <simon_freelance@hotmail.com>
2019-02-05 18:29:46 -0500

    Unique words appear in the hash map and values store duplicates of that word.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class Main {
 
@@ -24,6 +25,18 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         String text = readFile("C:\\Users\\Capt. Kirk\\IdeaProjects\\wordApp\\src\\rights.txt");
-        System.out.println(text);
+//        System.out.println(text);
+        String[] words = text.split("\\s+");
+        HashMap<String, Integer> uniqueMap = new HashMap<>();
+        for(int i = 0; i < words.length; i++) {
+            String word = words[i].toLowerCase();
+            if(uniqueMap.get(word) == null) {
+                uniqueMap.put(word, 1);
+                continue;
+            }
+                uniqueMap.put(word, uniqueMap.get(word) + 1);
+        }
+        System.out.println(uniqueMap);
+
     }
 }
